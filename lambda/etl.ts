eafba81709fe2e036304733c8e6789d5908c1296
Jairo Miranda <jmiranda@tridente.ai>
2025-05-18 16:18:35 -0600

    Initial commit that includes: cdk minimal configuration, lambda that will perform the etl process

---
+++ lambda/etl.ts
@@ -0,0 +1,4 @@
+export const handler = async () => {
+  console.log("ETL job running at midnight UTC");
+  // Your ETL logic here
+};
--- lib/etl-cdk-stack.ts
+++ lib/etl-cdk-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+
+import path from 'path';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EtlCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const etlFunction = new lambda.Function(this, 'ETLLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'etl.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
+    });
+    // EventBridge Rule to run every day at midnight UTC
+    const rule = new events.Rule(this, 'DailyETLRule', {
+      schedule: events.Schedule.cron({ minute: '0', hour: '0' }), // UTC 00:00
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EtlCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    rule.addTarget(new targets.LambdaFunction(etlFunction));
+
   }
 }
